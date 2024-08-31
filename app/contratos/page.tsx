dd7e17d569bd6c93063dee61dcbfda3ed6bbd0f5
Nicolas Jorge <nicolasjorgedl@hotmail.com>
2024-08-31 20:50:56 -0300

    iniciando pagia de contratos, adicionando components

---
+++ app/contratos/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TableContract from "./custom_component/table_contract";
+
+export default function Contratos() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 w-full">
+        <TableContract/>
+    </main>
+  );
+}
